a5ad9cc7a76abb5b3c29fe7c5922291838aa8e86
Luan Campos <luancamposproducoes@gmail.com>
2023-11-22 10:37:32 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import AuthContext from './context/AuthContext'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Entre | Cadastre-se',
+  description: 'Sua estante de livros virtual',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>{children}</AuthContext>
+      </body>
+    </html>
+  )
+}
