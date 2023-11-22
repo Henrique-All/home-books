b289eeaa13c5d65aa80aa920a1ca73fc12e42ff0
Luan Campos <luancamposproducoes@gmail.com>
2023-11-22 11:21:50 -0300

    chore: add material-symbols

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import 'material-symbols'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import AuthContext from './context/AuthContext'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
